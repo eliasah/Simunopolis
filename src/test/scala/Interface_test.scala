3680aeb805fc726298441c53e102eb0f1577964c
Christian Chiev <christian.chiev@gmail.com>
2013-11-27 01:27:17 +0100

    ajout des evenement souris

--- src/test/scala/Interface_test.scala
+++ src/test/scala/Interface_test.scala
@@ -3,22 +3,27 @@ package gui
 import scala.swing._
 import java.awt.Dimension
 import java.lang.reflect.Field
+import scala.swing.event.MouseClicked
+import scala.swing.event.MouseClicked
+import scala.swing.event.MouseClicked
 
 object test extends SimpleSwingApplication {
   val data = Array.ofDim[Color](50, 50)
+  var batiment = 0;
   def top = new MainFrame {
     title = "Simunopolis";
-    preferredSize_=(new Dimension(500, 500));
+    preferredSize_=(new Dimension(600, 600));
     var population: Int = 0;
     var budget: Int = 50000;
-    for (i: Int <- 0 to 49) {
+    /*for (i: Int <- 0 to 49) {
       for (j: Int <- 0 to 49) {
         if ((i + j) % 2 == 0)
-          data(i)(j) = java.awt.Color.magenta
+          data(i)(j) = java.awt.Color.lightGray
         else
           data(i)(j) = java.awt.Color.white;
       }
-    }
+    }*/
+
     var a = new FlowPanel() {
       var a = new FlowPanel() {
         contents += new Label("Population ")
@@ -28,13 +33,28 @@ object test extends SimpleSwingApplication {
       }
       contents += a;
       var graph = new DataPanel(data) {
-        preferredSize_=(new Dimension(450, 400))
+        listenTo(this.mouse.clicks);
+        preferredSize_=(new Dimension(500, 500));
+
+        reactions += {
+          case e: MouseClicked => {
+            println((e.point.getX() / 10).toInt);
+            println((e.point.getY() / 10).toInt);
+            var color=java.awt.Color.lightGray;
+            if(batiment==1){
+              color=java.awt.Color.blue;
+            }
+            data((e.point.getX() / 10).toInt)((e.point.getY() / 10).toInt)=color;
+            repaint;
+          }
+        };
       };
-      contents +=graph;
-      /*graph = new Action("Action Panel"){}
-			contents += graph*/
+      contents += graph;
+
+      contents += new Button(Action("Maison"){
+        batiment=1;
+      });
     }
-    //a.background = java.awt.Color.white;
     contents = a
   }
 }
