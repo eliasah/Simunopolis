2fb7e09ecb83903125c40cbde4ec95ccbe0eca4a
Christian Chiev <christian.chiev@gmail.com>
2013-12-02 17:14:49 +0100

    Abstraction des commandes de "construction/destruction" Ajout des boutons Commerce/Industrie/Destruction/Route Test sur le MouseDragged

--- src/main/scala/gui/GUI.scala
+++ src/main/scala/gui/GUI.scala
@@ -5,24 +5,32 @@ import java.awt.Dimension
 import scala.swing.event.MouseClicked
 import scala.swing.event.MouseClicked
 import scala.swing.event.MouseClicked
+import sun.nio.fs.AbstractAclFileAttributeView
+import scala.swing.event.MouseDragged
+import scala.swing.event.MouseReleased
 
 object GUI extends SimpleSwingApplication {
+  /**/
+  abstract class Command
+  case class Empty extends Command
+  case class House extends Command
+  case class Commerce extends Command
+  case class Industry extends Command
+  case class Road extends Command
+  case class Destroy extends Command
+
   val data = Array.ofDim[Color](50, 50)
-  var batiment = 0;
+  var command: Command = new Empty;
+  var x = 0;
+  var y = 0;
+  var xold = 0;
+  var yold = 0;
+  var color = java.awt.Color.WHITE;
   def top = new MainFrame {
     title = "Simunopolis";
     preferredSize_=(new Dimension(600, 600));
     var population: Int = 0;
     var budget: Int = 50000;
-    /*for (i: Int <- 0 to 49) {
-      for (j: Int <- 0 to 49) {
-        if ((i + j) % 2 == 0)
-          data(i)(j) = java.awt.Color.lightGray
-        else
-          data(i)(j) = java.awt.Color.white;
-      }
-    }*/
-
     var a = new FlowPanel() {
       var a = new FlowPanel() {
         contents += new Label("Population ")
@@ -33,26 +41,65 @@ object GUI extends SimpleSwingApplication {
       contents += a;
       var graph = new DataPanel(data) {
         listenTo(this.mouse.clicks);
+        listenTo(this.mouse.moves);
         preferredSize_=(new Dimension(500, 500));
 
         reactions += {
-          case e: MouseClicked => {
-            println((e.point.getX() / 10).toInt);
-            println((e.point.getY() / 10).toInt);
-            var color=java.awt.Color.lightGray;
-            if(batiment==1){
-              color=java.awt.Color.blue;
+          case d: MouseDragged => {
+            command match {
+              case r: Road => {
+                x = d.point.getX().toInt
+                y = d.point.getY().toInt
+              }
+              case _ =>
             }
-            data((e.point.getX() / 10).toInt)((e.point.getY() / 10).toInt)=color;
+          }
+          case e: MouseClicked => {
+            /* println((e.point.getX() / 10).toInt);
+            println((e.point.getY() / 10).toInt);*/
+            color = command match {
+              case x: House => java.awt.Color.blue
+              case y: Commerce => java.awt.Color.red
+              case z: Industry => java.awt.Color.yellow
+              case e: Empty => java.awt.Color.WHITE
+              case r: Road => java.awt.Color.LIGHT_GRAY
+              case _ => java.awt.Color.WHITE
+            }
+            xold = e.point.getX().toInt
+            yold = e.point.getY().toInt
+            data((e.point.getX() / 10).toInt)((e.point.getY() / 10).toInt) = color;
             repaint;
+          }
+          case e: MouseReleased => {
+            command match {
+              case r: Road => {
+                println("xold : " + xold)
+                println("yold : " + yold)
+                println("x : " + x)
+                println("y : " + y)
+              }
+              case _ =>
+            }
           }
         };
       };
       contents += graph;
-
-      contents += new Button(Action("Maison"){
-        batiment=1;
+      contents += new Button(Action("Maison") {
+        command = new House;
       });
+      contents += new Button(Action("Commerce") {
+        command = new Commerce;
+      });
+      contents += new Button(Action("Industrie") {
+        command = new Industry;
+      });
+      contents += new Button(Action("Destroy") {
+        command = new Destroy;
+      });
+      contents += new Button(Action("Route") {
+        command = new Road;
+      });
+
     }
     contents = a
   }
